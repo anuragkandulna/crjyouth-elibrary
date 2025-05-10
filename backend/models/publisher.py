6cc690bcda8ec520ad03da0e426264d8e802ab79
Anurag Kandulna <dev.ak@outlook.in>
2025-05-10 19:42:55 +0530

    updated log

--- backend/models/publisher.py
+++ backend/models/publisher.py
@@ -3,9 +3,10 @@ from sqlalchemy import String, Integer
 from models.base import Base
 from utils.my_logger import CustomLogger
 from constants.constants import OPS_LOG_FILE
+from constants.config import LOG_LEVEL
 
 
-LOGGER = CustomLogger(__name__, level=20, log_file=OPS_LOG_FILE).get_logger()
+LOGGER = CustomLogger(__name__, level=LOG_LEVEL, log_file=OPS_LOG_FILE).get_logger()
 
 
 class Publisher(Base):
