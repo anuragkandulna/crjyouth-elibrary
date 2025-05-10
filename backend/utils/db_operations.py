11ed411c4889e75a0f10a1f847a7c6cbb78a207d
Anurag Kandulna <dev.ak@outlook.in>
2025-05-10 19:43:02 +0530

    updated log

--- backend/utils/db_operations.py
+++ backend/utils/db_operations.py
@@ -8,9 +8,10 @@ from models.publisher import Publisher
 from models.library_office import LibraryOffice
 from utils.my_logger import CustomLogger
 from constants.constants import OPS_LOG_FILE
+from constants.config import LOG_LEVEL
 
 
-LOGGER = CustomLogger(__name__, level=20, log_file=OPS_LOG_FILE).get_logger()
+LOGGER = CustomLogger(__name__, level=LOG_LEVEL, log_file=OPS_LOG_FILE).get_logger()
 
 
 def seed_roles(session: Session, predefined_roles: dict) -> None:
