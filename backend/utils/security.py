44c4a9ca2dedb525546768c6768191f51ce5ea99
Anurag Kandulna <dev.ak@outlook.in>
2025-05-27 02:13:44 +0530

    added password validator

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,4 +1,5 @@
 from flask_argon2 import Argon2
+from zxcvbn import zxcvbn
 # from flask import current_app
 
 # Initialize Argon2 (to be called from app.py)
@@ -9,3 +10,7 @@ def generate_password_hash(password: str) -> str:
 
 def check_password_hash(hashed_password: str, password: str) -> bool:
     return argon2.check_password_hash(hashed_password, password)
+
+def is_strong_password(password: str, first_name: str, last_name: str, email: str, phone_number: str) -> bool:
+    result = zxcvbn(password=password, user_inputs=[first_name, last_name, email, phone_number])
+    return result['score'] > 2
