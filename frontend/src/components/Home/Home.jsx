957f35cd687e49c48f79cf41cba21f528622f60c
Anurag Kandulna <dev.ak@outlook.in>
2025-08-28 03:17:57 +0530

    added home

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -15,6 +15,7 @@ import {
     LockClosedIcon,
     ServerIcon,
 } from "@heroicons/react/20/solid";
+import library_room from "../../assets/room.jpg";
 
 const favorites = [
     {
@@ -54,122 +55,6 @@ function classNames(...classes) {
 export default function Home() {
     return (
         <>
-            {/* <main> */}
-            {/* Hero section */}
-            {/* Category section */}
-            {/* <section aria-labelledby="category-heading" className="bg-gray-50">
-                <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
-                    <div className="sm:flex sm:items-baseline sm:justify-between">
-                        <h2
-                            id="category-heading"
-                            className="text-2xl font-bold tracking-tight text-gray-900"
-                        >
-                            Shop by Category
-                        </h2>
-                        <a
-                            href="#"
-                            className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block"
-                        >
-                            Browse all categories
-                            <span aria-hidden="true"> &rarr;</span>
-                        </a>
-                    </div>
-
-                    <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
-                        <div className="group relative aspect-[2/1] overflow-hidden rounded-lg sm:row-span-2 sm:aspect-square">
-                            <img
-                                alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
-                                src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-featured-category.jpg"
-                                className="absolute size-full object-cover group-hover:opacity-75"
-                            />
-                            <div
-                                aria-hidden="true"
-                                className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"
-                            />
-                            <div className="absolute inset-0 flex items-end p-6">
-                                <div>
-                                    <h3 className="font-semibold text-white">
-                                        <a href="#">
-                                            <span className="absolute inset-0" />
-                                            New Arrivals
-                                        </a>
-                                    </h3>
-                                    <p
-                                        aria-hidden="true"
-                                        className="mt-1 text-sm text-white"
-                                    >
-                                        Shop now
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="group relative aspect-[2/1] overflow-hidden rounded-lg sm:aspect-auto">
-                            <img
-                                alt="Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters."
-                                src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-category-01.jpg"
-                                className="absolute size-full object-cover group-hover:opacity-75"
-                            />
-                            <div
-                                aria-hidden="true"
-                                className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"
-                            />
-                            <div className="absolute inset-0 flex items-end p-6">
-                                <div>
-                                    <h3 className="font-semibold text-white">
-                                        <a href="#">
-                                            <span className="absolute inset-0" />
-                                            Accessories
-                                        </a>
-                                    </h3>
-                                    <p
-                                        aria-hidden="true"
-                                        className="mt-1 text-sm text-white"
-                                    >
-                                        Shop now
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="group relative aspect-[2/1] overflow-hidden rounded-lg sm:aspect-auto">
-                            <img
-                                alt="Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk."
-                                src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-category-02.jpg"
-                                className="absolute size-full object-cover group-hover:opacity-75"
-                            />
-                            <div
-                                aria-hidden="true"
-                                className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"
-                            />
-                            <div className="absolute inset-0 flex items-end p-6">
-                                <div>
-                                    <h3 className="font-semibold text-white">
-                                        <a href="#">
-                                            <span className="absolute inset-0" />
-                                            Workspace
-                                        </a>
-                                    </h3>
-                                    <p
-                                        aria-hidden="true"
-                                        className="mt-1 text-sm text-white"
-                                    >
-                                        Shop now
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mt-6 sm:hidden">
-                        <a
-                            href="#"
-                            className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
-                        >
-                            Browse all categories
-                            <span aria-hidden="true"> &rarr;</span>
-                        </a>
-                    </div>
-                </div>
-            </section> */}
             <section>
                 {/* Hero section */}
                 <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
@@ -270,7 +155,7 @@ export default function Home() {
                     <div className="absolute inset-0 overflow-hidden">
                         <img
                             alt=""
-                            src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-feature-section-full-width.jpg"
+                            src={library_room}
                             className="size-full object-cover"
                         />
                     </div>
